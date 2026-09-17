8e85b22f5d225169261eb37b5996305047b64025
FernandoSerrano900 <00098725@uca.edu.sv>
2026-09-17 14:51:51 -0600

    :sparkles: feat: eliminar incidencias y agregar estadisticas

--- controllers/incidenciasController.js
+++ controllers/incidenciasController.js
@@ -78,3 +78,84 @@ function cambiarEstadoIncidencia(req, res) {
     incidencia,
   });
 }
+
+function eliminarIncidencia(req, res) {
+  const id = Number(req.params.id);
+
+  if (Number.isNaN(id)) {
+    return res.status(400).json({
+      mensaje: 'El id debe ser un número',
+    });
+  }
+
+  const indice = incidencias.findIndex(
+    (inc) => inc.id === id
+  );
+
+  if (indice === -1) {
+    return res.status(404).json({
+      mensaje: 'Incidencia no encontrada',
+    });
+  }
+
+  incidencias.splice(indice, 1);
+
+  return res.status(200).json({
+    mensaje: 'Incidencia eliminada correctamente',
+  });
+}
+
+function obtenerEstadisticas(req, res) {
+  const conteoPorEstado = incidencias.reduce(
+    (acumulador, incidencia) => {
+      acumulador[incidencia.estado] =
+        (acumulador[incidencia.estado] || 0) + 1;
+
+      return acumulador;
+    },
+    {}
+  );
+
+  return res.status(200).json({
+    totalIncidencias: incidencias.length,
+    pendientes: conteoPorEstado.Pendiente || 0,
+    enProceso: conteoPorEstado['En Proceso'] || 0,
+    resueltas: conteoPorEstado.Resuelta || 0,
+    canceladas: conteoPorEstado.Cancelada || 0,
+  });
+}
+
+function clasificarIncidencia(req, res) {
+  const id = Number(req.params.id);
+
+  const incidencia = incidencias.find(
+    (inc) => inc.id === id
+  );
+
+  if (!incidencia) {
+    return res.status(404).json({
+      mensaje: 'Incidencia no encontrada',
+    });
+  }
+
+  let clasificacion;
+
+  switch (incidencia.prioridad) {
+    case 'Alta':
+      clasificacion = 'Crítica';
+      break;
+    case 'Media':
+      clasificacion = 'Importante';
+      break;
+    case 'Baja':
+      clasificacion = 'Normal';
+      break;
+    default:
+      clasificacion = 'Sin clasificar';
+  }
+
+  return res.status(200).json({
+    id: incidencia.id,
+    clasificacion,
+  });
+}
